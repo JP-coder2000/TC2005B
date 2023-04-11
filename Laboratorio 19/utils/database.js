c7e9ecd91c687bd58124ae06b23c70fabb9999be
JP-coder2000 <70972830+JP-coder2000@users.noreply.github.com>
2023-04-11 09:17:52 -0600

    Subiendo ejercicios normalización

--- Laboratorio 19/utils/database.js
+++ Laboratorio 19/utils/database.js
@@ -4,8 +4,8 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'museo',
-  password: 'root',
-  port: '8889'
+  password: '',
+  port: 3306, // Puerto predeterminado de MySQL
 });
 
 module.exports = pool.promise();
